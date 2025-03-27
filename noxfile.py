204103702dbb4896fcf864b61b7f3dcb7ac21d75
ak1ra <git@ak1ra.xyz>
2025-03-27 15:16:27 +0800

    chore: add noxfile.py and ruff.toml

---
+++ noxfile.py
@@ -0,0 +1,18 @@
+import os
+
+import nox
+
+os.environ.update({"PDM_IGNORE_SAVED_PYTHON": "1"})
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    session.run_always("pdm", "install", "-G", "tests", external=True)
+    session.run("pytest", "-v", "tests/")
+
+
+@nox.session
+def lint(session: nox.Session) -> None:
+    session.run_always("pdm", "install", "-G", "lint", external=True)
+    session.run("ruff", "-v", "check", "--fix", "src/", "tests/")
+    session.run("ruff", "-v", "format", "src/", "tests/")
